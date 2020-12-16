a92c4e87117a7519e984f7e89cd13971cb8eb7c6
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2020-12-16 20:50:04 +0530

    update 2020

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+// const age = document.querySelectorAll('.age')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,3 +11,13 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+document.getElementById("age").innerHTML = calculateAge(new Date(1993, 09, 17));
+
+document.getElementById("experience").innerHTML = calculateAge(new Date(2016, 06, 20));
+
+function calculateAge(birthday) {
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
